efa924c270d6dc279c3b6d70f338e0d4abb85cc6
Tetiana2023 <drofaj18@gmail.com>
2023-01-18 11:07:26 +0200

    task -01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+  const buttonStart = document.querySelector("button[data-start]");
+  const buttonStop = document.querySelector("button[data-stop]")
+  const bodyEl = document.querySelector("body");
+
+  let timerId = null;
+
+
+
+  buttonStart.addEventListener('click', onButtonStartClick);
+
+  function onButtonStartClick() {
+     timerId = setInterval(()=> {
+       bodyEl.style.backgroundColor = getRandomHexColor() 
+    }, 1000);
+
+    buttonStart.disabled = true;
+  };
+  buttonStop.addEventListener('click', () =>{
+    clearInterval(timerId);
+    buttonStart.disabled = false;
+
+  } )
+
+ 
+
+    
+
+  
