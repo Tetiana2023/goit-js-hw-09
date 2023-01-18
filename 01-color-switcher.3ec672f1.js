31834be36d7f0ca6845b5c952823efe2a08f77d2
Tetiana2023 <112204151+Tetiana2023@users.noreply.github.com>
2023-01-18 09:07:52 +0000

    Deploying to gh-pages from @ Tetiana2023/goit-js-hw-09@efa924c270d6dc279c3b6d70f338e0d4abb85cc6 🚀

---
+++ 01-color-switcher.3ec672f1.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let n=null;t.addEventListener("click",(function(){n=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(n),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.3ec672f1.js.map
