3569692807c066a28f39bc5ba72ffba4f851561d
Tetiana2023 <drofaj18@gmail.com>
2023-01-22 14:13:41 +0200

    task - 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,4 @@
+import Notiflix from 'notiflix';
 
 const formEl = document.querySelector(".form");
 const delayEl = document.querySelector('[name="delay"]');
@@ -17,25 +18,28 @@ function onFormBtnSubmit(e) {
         createPromise(position, delay)
 
         .then(({ position, delay }) => {
-          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
         })
         .catch(({ position, delay }) => {
-          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
         });
+        delay += step;  
       }
-            
+         
     }
 
   function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-   return new Promise((resolve, reject) => {
-   
-      if (shouldResolve) {
+    const shouldResolve = Math.random() > 0.3;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
       resolve({ position, delay });
-  } else {
-   reject({ position, delay });
-  }
-  })
+      } else {
+        reject({ position, delay });
+      }
 
-    }
+      }, delay)
+         
+    })
+  }
   
